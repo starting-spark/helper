b11387f240367ed76af5d89c5c64cf46c85a88d6
Zhao Li <zhao.li@startingspark.com>
2018-04-15 17:17:29 -1000

    send request to IFTTT

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,27 @@
+require 'net/http'
+require 'uri'
+
 class HomeController < ApplicationController
+  IFTTT_TOKEN = ENV['IFTTT_TOKEN']
+
   def livingroom_light
+    uri = URI.parse("https://maker.ifttt.com/trigger/toggle_livingroom_light/with/key/#{IFTTT_TOKEN}")
+    response = Net::HTTP.get_response(uri)
+    puts response.code
+    puts response.body
   end
 
   def bedroom_light
+    uri = URI.parse("https://maker.ifttt.com/trigger/toggle_bedroom_light/with/key/#{IFTTT_TOKEN}")
+    response = Net::HTTP.get_response(uri)
+    puts response.code
+    puts response.body
   end
 
   def bedroom_ac
+    uri = URI.parse("https://maker.ifttt.com/trigger/toggle_bedroom_ac/with/key/#{IFTTT_TOKEN}")
+    response = Net::HTTP.get_response(uri)
+    puts response.code
+    puts response.body
   end
 end
